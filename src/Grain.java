6cc0f0b8f81517e64ceddcbb0adb4aec9a79a280
Gabriel Lapolla <gabrieljlapolla@protonmail.com>
2022-02-02 12:46:56 -0500

    fixed grain not appearing centered on cursor

--- src/Grain.java
+++ src/Grain.java
@@ -6,21 +6,25 @@ import java.util.Random;
 import javax.swing.JComponent;
 
 public class Grain extends JComponent {
-    private final Color COLOR = randColor();
-    private final int SIZE = randSize();
+    private final Color COLOR;
+    private final int SIZE;
     public double xCoord;
     public double yCoord;
     public Velocity velocity;
     
 
     public Grain(double xCoord, double yCoord) {
-        this.xCoord = xCoord;
-        this.yCoord = yCoord;
+        this.COLOR = randColor();
+        this.SIZE = randSize();
+        this.xCoord = xCoord - (this.SIZE / 2);
+        this.yCoord = yCoord - (this.SIZE / 2);
     }
 
     public Grain(Point p) {
-        this.xCoord = p.x;
-        this.yCoord = p.y;
+        this.COLOR = randColor();
+        this.SIZE = randSize();
+        this.xCoord = p.x - (this.SIZE / 2);
+        this.yCoord = p.y - (this.SIZE / 2);
     }
 
     public Color getColor() {
@@ -68,7 +72,8 @@ public class Grain extends JComponent {
      */
     public static int randSize() {
         Random random = new Random(); // Each grain is a random size
-        return random.nextInt(7) + 3;
+        //return random.nextInt(7) + 3;
+        return 25;
     }
 
     protected void paintComponent(Graphics g) {
